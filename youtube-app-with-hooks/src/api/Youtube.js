9ec7a7481bf66d3b0990de750ed2cf11aff703bb
Olgun KAYA <olgun.kaya@ericsson.com>
2020-10-07 16:22:11 +0300

    hooks and function components

--- youtube-app-with-hooks/src/api/Youtube.js
+++ youtube-app-with-hooks/src/api/Youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDAWZ2aAto_8DVivahufpCUo6MoutH4ePY";
+const KEY = "KEY";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
